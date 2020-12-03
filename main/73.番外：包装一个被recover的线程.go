2a33de750585b5c97d4abe243c4d4f8f08351130
Xiao <1109038583@qq.com>
2020-12-03 11:51:52 +0800

    包装goroutine使他panic无效

--- main/73.番外：包装一个被recover的线程.go
+++ main/73.番外：包装一个被recover的线程.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"sync"
+)
+
+func Go(x func()) {
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer func() {
+			wg.Done()
+			if e := recover(); e != nil {
+				log.Printf("%v\n", e)
+				wg.Done()
+			}
+		}()
+		x()
+	}()
+	wg.Wait()
+}
+
+func main() {
+	Go(tryPanic)
+	Go(noPanic)
+	fmt.Println("来了")
+}
+func tryPanic() {
+	panic("干他")
+}
+func noPanic() {
+	fmt.Println("不干他了")
+}
